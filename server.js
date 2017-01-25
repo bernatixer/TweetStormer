d91c1410cb6bfe461f12347a28b1df7b49f833a4
Arnau Buch <arnaubt@gmail.com>
2017-01-25 11:57:53 +0100

    Edited server.js and added index.ejs and css

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
 var express = require('express');
+var helmet = require('helmet');
+var bodyParser = require('body-parser');
+var chalk = require('chalk');
+var port = process.env.PORT || 5000;
+var app = express();
+
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+app.use(helmet());
+
+app.get('/', function (req, res) {
+    res.render('index')
+})
+
+app.get('*', function (req, res) {
+    res.render('error');
+})
+
+app.listen(port, function() {
+    console.log('Server listening at port ' + chalk.blue(port));
+})
