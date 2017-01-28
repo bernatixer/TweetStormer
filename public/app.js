38a8346e8bef9cd982abfda4ca847068cf8a931c
Arnau Buch <arnaubt@gmail.com>
2017-01-28 01:24:21 +0100

    WIP on the parser

--- public/app.js
+++ public/app.js
@@ -1,9 +1,16 @@
 $(document).ready(function main() {
-    var max_len = 140;
 
     function proc(text) {
+        var max_len = 140;
         var tweets = [];
-        
+        var arr_words = text.split();
+        var str = '';
+        for (var i = 0; i < arr_words; i++){
+            var temp = str.concat(arr_words[i]);
+            if (temp.length < 140){
+
+            }
+        }
         return tweets;
     }
 
@@ -13,13 +20,16 @@ $(document).ready(function main() {
             var arr_tweets = proc(text);
             var tweets = $('.wrapper');
             $('.row').children().hide(400);
-            for (var i = 0; i < tweets.length; i++){
-                tweets.append('<div class=\"tweet\">' + arr_tweets[i] + '<br /><p class=\"copy-tag\">Copy</p></div>');
+            for (var i = 0; i < arr_tweets.length; i++) {
+                console.log(arr_tweets[i].length);
+                if (arr_tweets[i] !== '' && arr_tweets[i] !== '\n') {
+                    tweets.append('<div class=\"tweet\"><p>' + arr_tweets[i] + '</p><br /><p class=\"copy-tag\">Copy</p></div>');
+                }
             }
         }
     })
 
-    $('#copy-tag').on('click', function(){
+    $('#copy-tag').on('click', function() {
 
     })
 });
